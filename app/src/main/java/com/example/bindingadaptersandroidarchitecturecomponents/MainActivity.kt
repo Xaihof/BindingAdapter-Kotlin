b9bd0c5cc47162f6b948df9cfb2d65d302997cac
XAIHOF <abdullahxaihof@gmail.com>
2024-03-28 17:30:32 +0500

    This is the first commit by XAIHOF.

---
+++ app/src/main/java/com/example/bindingadaptersandroidarchitecturecomponents/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.bindingadaptersandroidarchitecturecomponents
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.bindingadaptersandroidarchitecturecomponents.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val post = Post("Introduction To Kotlin", "Xoksis", "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+        binding.post = post
+    }
+}
---
+++ app/src/main/java/com/example/bindingadaptersandroidarchitecturecomponents/Post.kt
@@ -0,0 +1,3 @@
+package com.example.bindingadaptersandroidarchitecturecomponents
+
+data class Post(val title: String, val description: String, val url: String)
